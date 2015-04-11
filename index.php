38aada8b27f016ca277e3f648bf968d410d75d84
Maciej Latt <macieklatt@gmail.com>
2015-04-12 00:33:29 +0200

    Zaimplementowana obsługa strony bloga

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 <!--Treść listy wpisów z bloga-->
 <div class="container">
   <div class="row">
-    <div class="col-md-12">
-
+    <div class="col-md-8">
       <!--Generowanie listy wpisów z bloga-->
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         
@@ -35,12 +34,36 @@
           <hr>
 
       <?php endwhile; ?>
-      <!-- post navigation -->
+      <div class="pagination">
+		<?php
+		global $wp_query;
+
+		$big = 999999999; // need an unlikely integer
+
+		echo paginate_links( array(
+			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+			'format' => '?paged=%#%',
+			'current' => max( 1, get_query_var('paged') ),
+			'total' => $wp_query->max_num_pages
+		) );
+		?>
+	  </div>
       <?php else: ?>
       <!-- no posts found -->
       <?php endif; ?>
 		<div class="big-gap"></div>
     </div>
+	<div class="col-md-4">
+				<div id="sidebar">
+					<?php   /* Widgetized sidebar, if you have the plugin installed. */
+					if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
+					<div class="widget">
+						<h2>Domyślny widget</h2>
+						<p>Ten widget będzie się pokazywał gdy żaden inny widget nie zostanie dodany do paska.</p>
+					</div> <!-- end widget -->
+					<?php endif; ?>
+				</div> <!-- end sidebar -->
+			</div>
   </div>
 </div>
 
